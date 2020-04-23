d2112b8ea75b7253f23b4b609963adce278bd446
Sithu <cithukyaw@gmail.com>
2020-04-23 22:00:59 +0630

    Styling JobList component

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 import Joke from "./Joke";
+import './JokeList.css';
 
 const API_URL = 'https://icanhazdadjoke.com';
 
@@ -35,11 +36,17 @@ class JokeList extends Component {
   render() {
     return (
       <div className="JokeList">
-        <h1>Dad Jokes</h1>
+        <div className="JokeList-sidebar">
+          <h1 className="JokeList-title">
+            <span>Dad</span> Jokes
+          </h1>
+          <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" />
+          <button className="JokeList-getmore">New Jokes</button>
+        </div>
         <div className="JokeList-jokes">
           {
             this.state.jokes.map(j => (
-              <Joke id={j.id} joke={j.joke} />
+              <Joke key={j.id} id={j.id} joke={j.joke} />
             ))
           }
         </div>
